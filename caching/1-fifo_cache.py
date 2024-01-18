96e8e85cd2d755b6cef4753bef32ba7ef404ceed
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-01-18 21:41:55 +0300

    changes

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -40,19 +40,17 @@ class FIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        ''' self descriptive '''
-        if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                return
-
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                item_discarded = self.key_indexes.pop(0)
-                del self.cache_data[item_discarded]
-                print("DISCARD:", item_discarded)
-
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard oldest entry in cache to accommodate new entry. '''
+        if key is not None and item is not None:
             self.cache_data[key] = item
-            self.key_indexes.append(key)
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
         """

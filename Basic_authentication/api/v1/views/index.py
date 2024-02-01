48cdcf40d12342ab5380761891fe446c48cdb8b5
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-02-01 21:34:44 +0300

    unauthorised error

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -3,8 +3,6 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
-
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -12,8 +10,6 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
-
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -24,3 +20,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
+    """
+    abort(401)

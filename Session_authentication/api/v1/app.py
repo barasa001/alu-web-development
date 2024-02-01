09131e4ebe2692c3e45973c6f0f6ed2dc517234f
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-02-01 22:58:48 +0300

    indentation

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,11 +7,13 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
+
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -49,26 +51,26 @@ def before_request() -> str:
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-                      '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
-    if auth.current_user(request) is None:
     current_user = auth.current_user(request)
     if current_user is None:
         abort(403)
 
     request.current_user = current_user
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")

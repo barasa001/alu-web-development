48db6fccd17e05e5f8977a4eaff2fdad4da6b017
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-02-01 23:16:18 +0300

    Update users.py

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -4,8 +4,6 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-
-
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
@@ -14,8 +12,6 @@ def view_all_users() -> str:
     """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
-
-
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
@@ -35,10 +31,17 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == "me" and request.current_user is not None:
         return jsonify(request.current_user.to_json())
 
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        user = request.current_user
+        return jsonify(user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
 
+    if request.current_user is None:
+        abort(404)
     return jsonify(user.to_json())
 
 
@@ -58,8 +61,6 @@ def delete_user(user_id: str = None) -> str:
         abort(404)
     user.remove()
     return jsonify({}), 200
-
-
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
     """ POST /api/v1/users/
@@ -96,8 +97,6 @@ def create_user() -> str:
         except Exception as e:
             error_msg = "Can't create User: {}".format(e)
     return jsonify({'error': error_msg}), 400
-
-
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
     """ PUT /api/v1/users/:id

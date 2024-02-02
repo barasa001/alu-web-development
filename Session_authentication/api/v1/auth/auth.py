9e8234665152dcfe9374e458ef14287d45707076
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-02-02 08:37:51 +0300

    Update auth.py

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -15,28 +15,28 @@ class Auth():
         """ Return boolean """
         if path is None or excluded_paths is None or not len(excluded_paths):
             return True
-            
+
         if path[-1] != '/':
             path += '/'
         if excluded_paths[-1] != '/':
             excluded_paths += '/'
-            
+
         astericks = [stars[:-1]
                      for stars in excluded_paths if stars[-1] == '*']
         for stars in astericks:
             if path.startswith(stars):
                 return False
-                
+
         if path in excluded_paths:
             return False
         return True
-        
+
     def authorization_header(self, request=None) -> str:
         """ Request Flask object """
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers.get('Authorization')
-        
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Flask request object """
         return None

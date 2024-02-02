af31817b7ffdd032723cbbac37cb7e103f199ae7
barasa254 <101549153+barasa001@users.noreply.github.com>
2024-02-02 08:26:58 +0300

    Update app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,13 +7,15 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
+
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 AUTH_TYPE = getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "auth":
@@ -33,7 +35,31 @@ elif AUTH_TYPE == "session_db_auth":
     auth = SessionDBAuth()
 
 
+@app.before_request
+def bef_req():
+    """
+    Filter each request before it's handled by the proper route
+    """
+    if auth is None:
+        pass
+    else:
+        setattr(request, "current_user", auth.current_user(request))
+        excluded = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
+        if auth.require_auth(request.path, excluded):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description="Forbidden")
+
+
 @app.errorhandler(404)
+@ app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
@@ -41,6 +67,11 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Request unauthorized handler
+
+    
+@ app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """ Unauthorized handler
     """
@@ -48,13 +79,18 @@ def unauthorized_error(error) -> str:
 
 
 @app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Request unauthorized handler
+
+    
+@ app.errorhandler(403)
 def forbidden_error(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.before_request
+@ app.before_request
 def before_request() -> str:
     """ Before Request Handler
     Requests Validation
